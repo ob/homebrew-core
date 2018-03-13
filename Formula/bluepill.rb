a9235ddee9ce8af28f2730336c782593126e893b
Oscar Bonilla <6f6231@gmail.com>
2018-03-12 18:30:29 -0700

    Update to v2.2.0
    Bumping to latest version of Bluepill

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -1,8 +1,8 @@
 class Bluepill < Formula
   desc "iOS testing tool that runs UI tests using multiple simulators"
   homepage "https://github.com/linkedin/bluepill"
-  url "https://github.com/linkedin/bluepill/archive/v2.1.0.tar.gz"
-  sha256 "5cf831caa8af1c53060976e74b4998241e1a9d3fb299bfe644a2312e07b8762b"
+  url "https://github.com/linkedin/bluepill/archive/v2.2.0.tar.gz"
+  sha256 "4be8b441df6e5b6590a28fba43e30334680b45cb6a92f4944d62726e88bf252f"
   head "https://github.com/linkedin/bluepill.git"
 
   bottle do
@@ -11,7 +11,7 @@ class Bluepill < Formula
     sha256 "d5b0ea149d04431fddb399585d57bcd09faaa33572a4a5803ec2ac6a1cc29185" => :sierra
   end
 
-  depends_on :xcode => ["9.0", :build]
+  depends_on :xcode => ["9.2", :build]
 
   def install
     xcodebuild "-workspace", "Bluepill.xcworkspace",
